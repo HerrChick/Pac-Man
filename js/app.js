649b33ef47382bd5de9fcc6640416c53cdccfe78
HerrChick <elliott.chick@gmail.com>
2019-07-12 15:04:02 +0100

    Finished movement and collision

--- js/app.js
+++ js/app.js
@@ -1,1 +1,96 @@
-console.log('js loaded')
+document.addEventListener('DOMContentLoaded', () => {
+
+  const gridHeight = 18
+  const gridWidth = 18
+  const grid = document.querySelector('.grid')
+  let pacmanPosition = 18
+
+
+
+
+
+
+  const gameBoard =
+
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,0,0,0,0,2,2,2,2,2,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+  function drawGameBoard(){
+    for (let i = 0; i < gameBoard.length; i++)
+
+      if(gameBoard[i] === 1){
+        grid.innerHTML += '<div class= \'wall\'></div>'
+
+      } else if (gameBoard[i] === 0){
+        grid.innerHTML += '<div class= \'blank\'></div>'
+      }else if (gameBoard[i] === 2){
+        grid.innerHTML += '<div class= \'pellet blank\'></div>'
+      }
+
+  }
+
+
+
+
+
+  function movePacman(e) {
+
+    grid.children[pacmanPosition].classList.remove('pacman')
+
+    switch(e.keyCode) {
+      // moving left
+      case 37:
+        if (grid.children[pacmanPosition - 1].classList.contains('wall')) pacmanPosition += 0
+        else if (pacmanPosition % gridWidth !== 0) pacmanPosition -= 1
+        break
+        // moving up
+      case 38:
+        if (grid.children[pacmanPosition - gridWidth].classList.contains('wall')) pacmanPosition += 0
+        else if(pacmanPosition - gridWidth >= 0) pacmanPosition -= gridWidth
+        break
+        // moving right
+      case 39:
+        if (grid.children[pacmanPosition + 1].classList.contains('wall')) pacmanPosition += 0
+        else if(pacmanPosition % gridWidth < gridWidth - 1) pacmanPosition += 1
+        break
+        // moving down
+      case 40:
+        if (grid.children[pacmanPosition + gridWidth].classList.contains('wall')) pacmanPosition += 0
+        else if(pacmanPosition + gridWidth < gridWidth * gridWidth) pacmanPosition += gridWidth
+        break
+    }
+
+    if(grid.children[pacmanPosition].classList.contains('pellet')) {
+      grid.children[pacmanPosition].classList.remove('pellet')
+    }
+    grid.children[pacmanPosition].classList.add('pacman')
+  }
+
+
+
+  drawGameBoard()
+  document.addEventListener('keyup', movePacman)
+
+  console.log(grid.children)
+
+
+
+
+})
